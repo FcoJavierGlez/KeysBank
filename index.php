70c8a6d96ba50ebe0582f054638d00eb81b00c30
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-03 20:46:30 +0100

    First commit dev branch

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * @author Francisco Javier González Sabariego
+     */
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Francisco Javier González Sabariego">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/main.js"></script>
+        <title>Document</title>
+    </head>
+    <body>
+        <noscript><h1>Estapágina requiere el uso de JavaScript</h1></noscript>
+        <header></header>
+        <nav></nav>
+        <main>
+            <div class="anim">
+                <input type="text" name="" id="" placeholder="username">
+                <input type="password" name="" id="" placeholder="password">
+                <input type="submit" name="" value="Enter">
+            </div>
+        </main>
+        <footer></footer>
+    </body>
+</html>
