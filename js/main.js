22e2adce1218b8251b5a1adaf195de91e0256fc9
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-07 15:09:39 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 {
     const init = () => {
-        const LOGIN_SCREEN = [...document.getElementsByClassName("login_screen")];
+        const LOGIN_SCREEN = document.getElementById("login_screen");
+        LOGIN_SCREEN.addEventListener("animationend", function () {
+            this.className = "hidden";
+        })
     }
     document.addEventListener("DOMContentLoaded", init);
 }
