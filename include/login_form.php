db4ccfb876bea70397e74428ddc21e94b445bf22
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-08 14:54:17 +0100

    Updated login-form

--- include/login_form.php
+++ include/login_form.php
@@ -1,10 +1,10 @@
 <?php
     echo "<form action='index.php' method='POST' class='form_login'>";
-        echo "<input type='text' name='username' placeholder='username' class=".inputStyle($errorLogin,$loggedNow)." value=".
-        (isset($_POST['username']) ? dataClean($_POST['username']) : '').">";
+        echo "<input type='text' name='nick' placeholder='nick' class=".inputStyle($errorLogin,$loggedNow)." value=".
+        (isset($_POST['nick']) ? dataClean($_POST['nick']) : '').">";
         echo "<input type='password' name='pswd' placeholder='password' class=".inputStyle($errorLogin,$loggedNow).">";
-        echo "<span class='text-error'>".($errorLogin ? 'Usuario y/o contraseñas incorrectos' : '')."</span>";
+        echo "<span class='text-error'>".($errorLogin ? 'Usuario y/o contraseña incorrectos' : '')."</span>";
         echo "<input type='submit' name='login' value='Enter' class=''>";
-        echo "<a href=".$_SERVER['PHP_SELF'].'?page=register'." class='register'>Register now</a>";
+        echo "<a href=".$_SERVER['PHP_SELF'].'?register'." class='register'>Register now</a>";
     echo "</form>";
 ?>
