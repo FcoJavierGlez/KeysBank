2f8100b18e2e8063de04931168d06f704106b22f
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-08 22:03:22 +0100

    Added Plataforms Class

---
+++ class/Plataforms.php
@@ -0,0 +1,36 @@
+<?php
+
+    class Plataforms extends DBAbstractModel {
+
+        private static $_instancia;
+
+        public function __construct() {
+
+        }
+        
+        public static function singleton() {
+            if (!isset(self::$_instancia)) {
+                $miClase = __CLASS__;
+                self::$_instancia = new $miClase;
+            }
+            return self::$_instancia;
+        }
+
+        public function __clone() {
+            trigger_error('La clonación no es permitida.', E_USER_ERROR);
+        }
+
+        /**
+         * Devuelve el número de plataformas
+         */
+        public function getTotalNumberPlataforms() {
+            $this->query = "SELECT id FROM keysbank_plataforms";
+
+            $this->get_results_from_query();
+            $this->close_connection();
+
+            return sizeof($this->rows);
+        }
+    }
+    
+?>
