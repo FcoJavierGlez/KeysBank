0de0d6171d509f1c084a3e3d7772fc1f0c3d2496
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-09 17:11:13 +0100

    Updated nav.php into include folder

--- include/nav.php
+++ include/nav.php
@@ -1,15 +1,40 @@
 <?php
     $user_nav = array(
         array(
-            'name' => 'My accounts',
+            'name' => 'Accounts',
             'url' => 'accounts.php'
         ),
         array(
-            'name' => 'Edit perfil',
+            'name' => 'Perfil',
             'url' => 'perfil.php'
         ),
-    )
+    );
+    $admin_nav = array(
+        array(
+            'name' => 'Users',
+            'url' => 'users.php'
+        ),
+        array(
+            'name' => 'Plataforms',
+            'url' => 'plataforms.php'
+        ),
+        array(
+            'name' => 'Perfil',
+            'url' => 'perfil.php'
+        ),
+    );
     
-    
+    if (!($_SESSION['user']['perfil'] == 'INVITED' || $_SESSION['user']['current_state'] !== 'ACTIVE')) {
+        echo "<ul>";
+        echo "<li><a href='".(preg_match('/\/pages\//', $_SERVER['PHP_SELF']) ? '../index.php' : 'index.php')."'>Home</a></li>";
+        if ($_SESSION['user']['perfil'] == 'ADMIN') 
+            foreach ($admin_nav as $value)
+                echo "<li><a href='".(preg_match('/\/pages\//', $_SERVER['PHP_SELF']) ? $value['url'] : 'pages/'.$value['url'])."'>".$value['name']."</a></li>";
+        else
+            foreach ($user_nav as $value)
+                echo "<li><a href='".(preg_match('/\/pages\//', $_SERVER['PHP_SELF']) ? $value['url'] : 'pages/'.$value['url'])."'>".$value['name']."</a></li>";
+        echo "</ul>";
+
+    }
 
 ?>
