9601072c91d591786f2646e1a15ce528d5d3cf82
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-09 13:38:36 +0100

    Added nav.php into include folder

---
+++ include/nav.php
@@ -0,0 +1,15 @@
+<?php
+    $user_nav = array(
+        array(
+            'name' => 'My accounts',
+            'url' => 'accounts.php'
+        ),
+        array(
+            'name' => 'Edit perfil',
+            'url' => 'perfil.php'
+        ),
+    )
+    
+    
+
+?>
