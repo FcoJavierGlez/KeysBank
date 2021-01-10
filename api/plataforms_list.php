52681c96217b542c8f9ed735b56bc2ea43abbf7c
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-10 15:55:17 +0100

    Added subcategories_list and plataforms_list files into api folder. Updated plataform_categories_list file into api folder

--- api/plataforms_list.php
+++ api/plataforms_list.php
@@ -2,17 +2,17 @@
     /**
      * @author Francisco Javier González Sabariego
      * 
-     * Devuelve la lista con las categorías de plataformas.
+     * Devuelve la lista con las plataformas correspondientes a la categoría elegida.
      */
     header("Access-Control-Allow-Origin: *");
-    
+        
     include "../config/config_dev.php";
     include "../resource/functions.php";
     include "../class/DBAbstractModel.php";
     include "../class/Plataforms.php";
 
     $plataforms = Plataforms::singleton();
-    $categoriesList = $plataforms->getPlataformCategories();
+    $plataformsList = $plataforms->getPlataformsByCategory($_POST['categories']);
 
-    print_r(json_encode($categoriesList));
+    print_r(json_encode($plataformsList));
 ?>
--- api/subcategories_list.php
+++ api/subcategories_list.php
@@ -2,17 +2,21 @@
     /**
      * @author Francisco Javier González Sabariego
      * 
-     * Devuelve la lista con las categorías de plataformas.
+     * Devuelve la lista con las subcategorías de la categoría elegida.
      */
     header("Access-Control-Allow-Origin: *");
-    
+        
     include "../config/config_dev.php";
     include "../resource/functions.php";
     include "../class/DBAbstractModel.php";
     include "../class/Plataforms.php";
 
     $plataforms = Plataforms::singleton();
-    $categoriesList = $plataforms->getPlataformCategories();
+    $subcategoriesList = $plataforms->getSubcategoriesList($_POST['categories']);
 
-    print_r(json_encode($categoriesList));
+    print_r(json_encode($subcategoriesList));
+
+    /* $plataformSelected = $_POST['plataforms'];
+
+    echo json_encode($plataformSelected); */
 ?>
