49e755387704a2f31ef9e63b375861c424b701a1
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-12 10:48:51 +0100

    Added copyValue() function

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,11 @@
 const normalizeOption = input => input.replace(/_/g, " ").replace( /\b([\w])/g, e => e.toUpperCase() ).replace(/\//g, " / ");
+
+const copyValue = textToCopy => {  //I must move another js file
+    const HIDDEN = document.createElement("input");
+    HIDDEN.type  = "text";
+    HIDDEN.value = textToCopy;
+    document.body.appendChild(HIDDEN);
+    HIDDEN.select();
+    document.execCommand("copy");
+    document.body.removeChild(HIDDEN);
+}
