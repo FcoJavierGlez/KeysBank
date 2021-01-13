06a2bc09d4d8b3633f3523230ea36c10aede82e6
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-13 21:10:32 +0100

    Removed function copyPass

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,2 @@
 const normalizeOption = input => input.replace(/_/g, " ").replace( /\b([\w])/g, e => e.toUpperCase() ).replace(/\//g, " / ");
 
-const copyValue = textToCopy => {  //I must move this to another js file
-    const HIDDEN = document.createElement("input");
-    HIDDEN.type  = "text";
-    HIDDEN.value = textToCopy;
-    document.body.appendChild(HIDDEN);
-    HIDDEN.select();
-    document.execCommand("copy");
-    document.body.removeChild(HIDDEN);
-}
