393713381def8babfbb5980236febeba1440163b
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-13 21:12:28 +0100

    Updated accounts_controller. Added view and controller 'view'

--- controller/accounts_controller.php
+++ controller/accounts_controller.php
@@ -3,11 +3,16 @@
     $result_search = array();
     $emptyList = FALSE;
     
-    if (isset($_POST['search_account'])) 
-        $result_search = $_SESSION['instance_accounts']->getUserAccounts( $_SESSION['user']['id'],strtolower( dataClean($_POST['input_search']) ) );
+    
+    if (isset($_GET['view'])) 
+        $result_search = $_SESSION['instance_accounts']->getAccountById($_SESSION['user']['id'],$_GET['view']);
     else {
-        $result_search = $_SESSION['instance_accounts']->getUserAccounts($_SESSION['user']['id'],'');
-        $emptyList = !sizeof($result_search);
+        if (isset($_POST['search_account'])) 
+            $result_search = $_SESSION['instance_accounts']->getUserAccounts( $_SESSION['user']['id'],strtolower( dataClean($_POST['input_search']) ) );
+        else {
+            $result_search = $_SESSION['instance_accounts']->getUserAccounts($_SESSION['user']['id'],'');
+            $emptyList = !sizeof($result_search);
+        }
     }
 
     //  VIEWS  \\
