12c9d0dcc25d05068cca4dfb73c1b095a1d1c72a
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-13 10:43:23 +0100

    Updated accounts_controller

--- controller/accounts_controller.php
+++ controller/accounts_controller.php
@@ -1,5 +1,20 @@
 <?php
+    // ACTIONS CONTROLLER \\
+    $result_search = array();
+    $emptyList = FALSE;
+    
+    if (isset($_POST['search_account'])) 
+        $result_search = $_SESSION['instance_accounts']->getUserAccounts( $_SESSION['user']['id'],strtolower( dataClean($_POST['input_search']) ) );
+    else {
+        $result_search = $_SESSION['instance_accounts']->getUserAccounts($_SESSION['user']['id'],'');
+        $emptyList = !sizeof($result_search);
+    }
+
+    //  VIEWS  \\
     if (isset($_GET['add'])) {
+        
+    }
+    elseif (isset($_GET['view'])) {
         
     }
     elseif (isset($_GET['edit'])) {
