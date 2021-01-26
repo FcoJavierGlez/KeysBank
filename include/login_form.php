7f13450434545c87180ef6d1d58c077341db7095
FcoJavierGlez <FJGS_86@hotmail.com>
2021-01-26 14:37:06 +0100

    Updated

--- include/login_form.php
+++ include/login_form.php
@@ -1,8 +1,8 @@
 <?php
     echo "<form action='index.php' method='POST' class='form_login'>";
-        echo "<input type='text' name='nick' placeholder='nick' class=".inputStyle($errorLogin,$loggedNow)." value=".
-        (isset($_POST['nick']) ? dataClean($_POST['nick']) : '').">";
-        echo "<input type='password' name='pswd' placeholder='password' class=".inputStyle($errorLogin,$loggedNow).">";
+        echo "<input type='text' name='nick' placeholder='nick' class='".inputStyle($errorLogin,$loggedNow)."' value='".
+        (isset($_POST['nick']) ? dataClean($_POST['nick']) : '')."'>";
+        echo "<input type='password' name='pswd' placeholder='password' class='".inputStyle($errorLogin,$loggedNow)."'>";
         echo "<span class='text-error'>".($errorLogin && !($userPending || $userBanned) ? 'Username or password invalid' : '')."</span>";
         echo "<span class='text-error'>".($userPending ? 'Account pending to be activated' : '')."</span>";
         echo "<span class='text-error'>".($userBanned ? 'Your account has been banned' : '')."</span>";
