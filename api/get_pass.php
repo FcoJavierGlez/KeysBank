372c07351606d1a2446f5ac80eb335f945ca9311
FcoJavierGlez <FJGS_86@hotmail.com>
2021-03-31 12:33:43 +0200

    Updated api files. Added message 'error 404'

--- api/get_pass.php
+++ api/get_pass.php
@@ -11,13 +11,29 @@
     include "../class/DBAbstractModel.php";
     include "../class/Accounts.php";
 
-    $account = Accounts::singleton();
-    $error   = array(array('AES_DECRYPT(UNHEX(A.name_account),K.password)' => '404'));
+    if (!isset($_POST['search'])) {
+        //header('Location:../index.php');
+        echo "<h1 style='margin: 21.44px 0'>¡Objeto no localizado!</h1>";
+        echo "<p style='margin: 16px 0 16px 48px'>No se ha localizado la URL solicitada en este servidor. 
+            Si usted ha introducido la URL manualmente, por favor revise su ortografía e inténtelo de nuevo.</p>";
+        echo "<p style='margin: 16px 0 16px 48px'>Si usted cree que esto es un error del servidor, por favor comuníqueselo al
+            <a href='mailto:postmaster@localhost'>administrador del portal</a></p>";
+        echo "<h2>Error 404</h2>";
+        echo "<address style='margin: 16px 0 16px 48px'>
+                <a href='/'>localhost</a><br>
+                <span style='font-size: smaller'>Apache/2.4.41 (Win64) OpenSSL/1.1.1c PHP/7.4.1</span>
+            </address>";
 
-    preg_match('/^\?(\d+)\!(\d+)$/', $_POST['search'], $matches);
+    } else {
+        $account = Accounts::singleton();
 
-    if (sizeof($matches)) 
-        print_r( json_encode($account->getPassAccountById($matches[1],$matches[2])) );
+        preg_match('/^\?(\d+)\!(\d+)$/', $_POST['search'], $matches);
+
+        if (sizeof($matches)) 
+            print_r( json_encode($account->getPassAccountById($matches[1],$matches[2])) );
+    }
+
+    
 
 
     
--- api/subcategories_list.php
+++ api/subcategories_list.php
@@ -11,8 +11,24 @@
     include "../class/DBAbstractModel.php";
     include "../class/Platforms.php";
 
-    $platforms = Platforms::singleton();
-    $subcategoriesList = $platforms->getSubcategoriesList($_POST['categories']);
+    if (!isset($_POST['categories'])) {
+        //header('Location:../index.php');
+        echo "<h1 style='margin: 21.44px 0'>¡Objeto no localizado!</h1>";
+        echo "<p style='margin: 16px 0 16px 48px'>No se ha localizado la URL solicitada en este servidor. 
+            Si usted ha introducido la URL manualmente, por favor revise su ortografía e inténtelo de nuevo.</p>";
+        echo "<p style='margin: 16px 0 16px 48px'>Si usted cree que esto es un error del servidor, por favor comuníqueselo al
+            <a href='mailto:postmaster@localhost'>administrador del portal</a></p>";
+        echo "<h2>Error 404</h2>";
+        echo "<address style='margin: 16px 0 16px 48px'>
+                <a href='/'>localhost</a><br>
+                <span style='font-size: smaller'>Apache/2.4.41 (Win64) OpenSSL/1.1.1c PHP/7.4.1</span>
+            </address>";
 
-    print_r(json_encode($subcategoriesList));
+    } else {
+        $platforms = Platforms::singleton();
+        $subcategoriesList = $platforms->getSubcategoriesList($_POST['categories']);
+    
+        print_r(json_encode($subcategoriesList));
+    }
+
 ?>
