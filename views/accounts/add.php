913b82f9f7b18557b07babe25e99ab473706be2b
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-02 01:49:57 +0200

    Updated add account view

--- views/accounts/add.php
+++ views/accounts/add.php
@@ -12,23 +12,64 @@
             </div> -->
             <div class='basic-info'>
                 <form id="form-add">
-                <fieldset class="select_platform">
-                    <legend>Category - subcategory</legend>
-                    <div class="div_select">
-                        <div>
-                            Categories:
+                    <fieldset class="select_platform">
+                        <legend>Platform</legend>
+                        <div class="div_select">
+                            <div>
+                                Categories:
+                            </div>
+                            <select name="categories" id="categories"></select>
                         </div>
-                        <select name="categories" id="categories"></select>
-                    </div>
-                    <div class="div_select">
-                        <div>
-                            Subcategories: 
+                        <div class="div_select">
+                            <div>
+                                Platforms: 
+                            </div>
+                            <select name="subcategories" id="subcategories">
+                                <option value="">-- Choice an option --</option>
+                            </select>
                         </div>
-                        <select name="subcategories" id="subcategories">
-                            <option value="">-- Choice an option --</option>
-                        </select>
-                    </div>
-                </fieldset>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Account data</legend>
+                        <div class="div_select">
+                            <div>
+                                Account name:
+                            </div>
+                            <input type="text" name="name" id="name">
+                            <span></span>
+                        </div>
+                        <fieldset class="password">
+                            <legend>Password</legend>
+                            <div class="div_select">
+                                <div>
+                                    Password:
+                                </div>
+                                <div class="div_pass">
+                                    <div id="shps" class="eye">
+                                        <input type="checkbox" name="shps">
+                                    </div>
+                                    <input type="password" name="pass" id="pass">
+                                </div>
+                                <div class="spacial_message">
+                                    <span id="strong_password" class="strongest">
+                                        Strongest
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="div_select">
+                                <div>
+                                    Repeat password:
+                                </div>
+                                <div class="div_pass">
+                                    <div id="shpsr" class="eye">
+                                        <input type="checkbox" name="shpsr">
+                                    </div>
+                                    <input type="password" name="pass_rep" id="pass_rep">
+                                </div>
+                                <span></span>
+                            </div>
+                        </fieldset>
+                    </fieldset>
                     <!-- <input type="hidden" name="search" value="<?php echo '?'.$_SESSION['user']['id'].'!'.$_GET['view']; ?>">
                     <div>
                         <b><u>Account</u>:</b>
