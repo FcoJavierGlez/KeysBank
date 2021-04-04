fead6d9b2a838dbab70a219ac7fea1a92e824756
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-04 23:15:37 +0200

    Added icon alternative

--- views/accounts/view.php
+++ views/accounts/view.php
@@ -1,32 +1,5 @@
 <?php
-    /* echo "<section>";
-    echo "<div class='panel-title'>";
-        echo "<div></div>";
-        echo "<h3>VIEW ACCOUNT</h3>";
-        echo "<div><a href='".$_SERVER['PHP_SELF'].'?add'."'><button class='accept'>Add account</button></a></div>";
-    echo "</div>";
-    echo "<div class='search'>";
-        echo "<form action='".$_SERVER['PHP_SELF']."' method='POST'>";
-            echo "<input type='text' name='input_search' id='' placeholder='Search plataform'>";
-            echo "<input type='submit' name='search_account' value='Search'>";
-        echo "</form>";
-    echo "</div>";
-    echo "<div class='result scroll'>";
-        echo "<article>";
-            echo "<div class='platform'>";
-                echo "<img src='../img/platform/".normalizeString($result_search[0]['name_platform']).".png' alt='Logo Twitter'>";
-                echo "<h3>".$result_search[0]['name_platform'].":</h3>";
-            echo "</div>";
-            echo "<div class='basic-info'>";
-                echo "<div><b><u>Account</u>:</b></div>";
-                echo "<div><span>".$result_search[0]['AES_DECRYPT(UNHEX(A.name_account),K.password)']."</span></div>";
-            echo "</div>";
-        echo "</article>";
-        echo "<pre>";
-            print_r($result_search);
-        echo "</pre>";
-    echo "</div>";
-    echo "</section>"; */
+    $route = file_exists("../img/platform/".normalizeString($result_search[0]['name_platform']).".png") ? "../img/platform/".normalizeString($result_search[0]['name_platform']).".png" : "../img/platform/other.png";
 ?>
 <section>
     <div class='panel-title'>
@@ -37,7 +10,7 @@
     <div class='result scroll'>
         <article>
             <div class='platform'>
-                <img src="<?php echo '../img/platform/'.normalizeString($result_search[0]['name_platform']).'.png'; ?>" alt="<?php echo 'Logo '.$result_search[0]['name_platform']; ?>">
+                <img src="<?php echo $route; ?>" alt="<?php echo 'Logo '.$result_search[0]['name_platform']; ?>">
                 <h3><?php echo $result_search[0]['name_platform']; ?>:</h3>
             </div>
             <div class='basic-info'>
