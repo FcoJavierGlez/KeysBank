92fdcf2cb7328e128dd870b0b795329373a8fc39
FcoJavierGlez <FJGS_86@hotmail.com>
2021-05-18 23:22:41 +0200

    Fixed link to index from logout button

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@
 <div class="close-session">
     <?php
         if ($_SESSION['user']['perfil'] !== 'INVITED') {    //Si el usuario ya está logeado se carga el botón de logout
-            echo "<form action='index.php' method='post'>";
+            echo "<form action='".(preg_match('/\/pages\//', $_SERVER['PHP_SELF']) ? '../index.php' : 'index.php')."' method='post'>";
                 echo "Welcome ".$_SESSION['user']['nick'].". ";
                 echo "<input type='submit' name='exit' value='Logout' class=''>";
             echo "</form>";
