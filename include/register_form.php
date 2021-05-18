bceb59c41ccf00ba042f494fda795c0218a693b8
FcoJavierGlez <FJGS_86@hotmail.com>
2021-05-18 21:22:02 +0200

    Changed view from php to html sintax

--- include/register_form.php
+++ include/register_form.php
@@ -28,37 +28,32 @@
         catch (MailExistException $mailExistException) {}
         catch (PassCheckException $passCheckException) {}
     }
-
-    if ($registered) {
-        echo "<div class='container-box'>";
-            echo "<div class='registered-box'>";
-            echo "<p>Succefully registered.</p>";
-            echo "<p>Your account is pending to activated.</p>";
-            echo "<a href=".$_SERVER['PHP_SELF']."><button>Back to login</button></a>";
-            echo "</div>";
-        echo "</div>";
-    }
-    else {
-        echo "<form action=".$_SERVER['PHP_SELF'].'?register'." method='POST' class='form_login'>";
-
-            echo "<input type='text' name='nick' placeholder='nick (*)' class='".($userExistException ? 'error' : 'required')."'
-                value='".(isset($_POST['add_user']) ? $_POST['nick'] : '')."' required>";
-            echo "<span class='text-error'>".($userExistException ? 'Nick is not available.' : '')."</span>";
-
-            echo "<input type='password' name='pswd' placeholder='password (*)' class='".($passCheckException ? 'error' : 'required')."' value='' required>";
-            echo "<input type='password' name='pswd2' placeholder='repeat password (*)' class='".($passCheckException ? 'error' : 'required')."' value='' required>";
-            echo "<span class='text-error'>".($passCheckException ? 'Passwords must match.' : '')."</span>";
-
-            echo "<input type='text' name='name' placeholder='name' value='".(isset($_POST['add_user']) ? $_POST['name'] : '')."'>";
-            echo "<input type='text' name='surname' placeholder='surname' value='".(isset($_POST['add_user']) ? $_POST['surname'] : '')."'>";
-
-            echo "<input type='email' name='email' placeholder='email (*)' class='".($mailFormatException || $mailExistException ? 'error' : 'required')."' 
-                value='".(isset($_POST['add_user']) ? $_POST['email'] : '')."' required>";
-            echo "<span class='text-error'>".($mailFormatException ? 'Email format error.' : '')."</span>";
-            echo "<span class='text-error'>".($mailExistException ? 'This email has already been registered.' : '')."</span>";
-
-            echo "<input type='submit' name='add_user' value='Register' class=''>";
-            echo "<div><a href=".$_SERVER['PHP_SELF']." class='back-login'>Back to login</a></div>";
-        echo "</form>";
-    }
 ?>
+<div class="<?php echo $registered ? 'container-box' : 'hidden' ?>">
+    <div class='registered-box'>
+    <p>Succefully registered.</p>
+    <p>Your account is pending to activated.</p>
+    <a href="<?php echo $_SERVER['PHP_SELF']; ?>"><button>Back to login</button></a>
+    </div>
+</div>
+
+<form action="index.php?register" method="POST" class="<?php echo $registered ? 'hidden' : 'form_login'; ?>">
+    <input type="text" name="nick" placeholder="nick (*)" class="<?php echo ($userExistException ? 'error' : 'required'); ?>"
+        value="<?php echo ( isset($_POST['add_user']) ? $_POST['nick'] : '' ); ?>" required>
+    <span class="text-error"><?php echo ($userExistException ? 'Nick is not available.' : ''); ?></span>
+
+    <input type="password" name="pswd" placeholder="password (*)" class="<?php echo ($passCheckException ? 'error' : 'required'); ?>" value="" required>
+    <input type="password" name="pswd2" placeholder="repeat password (*)" class="<?php echo ($passCheckException ? 'error' : 'required'); ?>" value='' required>
+    <span class="text-error"><?php echo ($passCheckException ? 'Passwords must match.' : ''); ?></span>
+
+    <input type="text" name="name" placeholder="name" value="<?php echo (isset($_POST['add_user']) ? $_POST['name'] : ''); ?>">
+    <input type="text" name="surname" placeholder="surname" value="<?php echo (isset($_POST['add_user']) ? $_POST['surname'] : ''); ?>">
+
+    <input type="email" name="email" placeholder="email (*)" class="<?php echo ($mailFormatException || $mailExistException ? 'error' : 'required'); ?>" 
+        value="<?php echo (isset($_POST['add_user']) ? $_POST['email'] : ''); ?>" required>
+    <span class="text-error"><?php echo ($mailFormatException ? 'Email format error.' : ''); ?></span>
+    <span class="text-error"><?php echo ($mailExistException ? 'Email already registered.' : ''); ?></span>
+
+    <input type="submit" name="add_user" value="Register" class="">
+    <div><a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="back-login">Back to login</a></div>
+</form>
