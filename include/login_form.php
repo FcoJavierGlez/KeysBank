6fbd08f9f79940c11e23fca11f572d362f1e82b5
FcoJavierGlez <FJGS_86@hotmail.com>
2021-05-18 21:30:52 +0200

    Changed view from php to html sintax

--- include/login_form.php
+++ include/login_form.php
@@ -1,12 +1,10 @@
-<?php
-    echo "<form action='index.php' method='POST' class='form_login'>";
-        echo "<input type='text' name='nick' placeholder='nick' class='".inputStyle($errorLogin,$loggedNow)."' value='".
-        (isset($_POST['nick']) ? dataClean($_POST['nick']) : '')."'>";
-        echo "<input type='password' name='pswd' placeholder='password' class='".inputStyle($errorLogin,$loggedNow)."'>";
-        echo "<span class='text-error'>".($errorLogin && !($userPending || $userBanned) ? 'Username or password invalid' : '')."</span>";
-        echo "<span class='text-error'>".($userPending ? 'Account pending to be activated' : '')."</span>";
-        echo "<span class='text-error'>".($userBanned ? 'Your account has been banned' : '')."</span>";
-        echo "<input type='submit' name='login' value='Enter' class=''>";
-        echo "<div><a href=".$_SERVER['PHP_SELF'].'?register'." class='register'>Register now</a></div>";
-    echo "</form>";
-?>
+<form action="index.php" method="POST" class="form_login">
+    <input type="text" name="nick" placeholder="nick" class="<?php echo inputStyle($errorLogin,$loggedNow); ?>" 
+            value="<?php echo (isset($_POST['nick']) ? dataClean($_POST['nick']) : ''); ?>">
+    <input type="password" name="pswd" placeholder="password" class="<?php echo inputStyle($errorLogin,$loggedNow); ?>">
+    <span class="text-error"><?php echo ($errorLogin && !($userPending || $userBanned) ? 'Username or password invalid' : ''); ?></span>
+    <span class="text-error"><?php echo ($userPending ? 'Account pending to be activated' : ''); ?></span>
+    <span class="text-error"><?php echo ($userBanned ? 'Your account has been banned' : ''); ?></span>
+    <input type="submit" name="login" value="Enter" class="">
+    <div><a href="index.php?register" class="register">Register now</a></div>
+</form>
