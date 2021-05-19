3719a718780f02c4dc8528c918bf67c027ec3ce4
FcoJavierGlez <FJGS_86@hotmail.com>
2021-05-19 11:08:37 +0200

    Updated

--- controller/platform_controller.php
+++ controller/platform_controller.php
@@ -42,18 +42,20 @@
                 $actionFailed = TRUE;
         }
     }
-    elseif (isset($_GET['edit'])) {
+    elseif (isset($_GET['edit'])) { //Vista editar plataforma
         $search_platform = $_SESSION['instance_platforms']->getPlatformById( $_GET['edit'] );
 
-        if (!sizeof($search_platform)) header('Location:./platforms.php');
+        if (!sizeof($search_platform)) header('Location:./platforms.php');  //Si no existe la plataforma redirigimos a la vista main
 
+        //Obtenemos la lista de subcategorías para la categoría de la plataforma
         $subcategories = $_SESSION['instance_platforms']->getSubcategoriesList( $_SESSION['instance_platforms']->getCategoryIdByName( $search_platform[0]['category'] ) );
         
+        //Cargamos el nombre del logo de la plataforma (si no existe usamos el logo por defecto 'other.png')
         $logo = file_exists("../img/platform/".normalizeString($search_platform[0]['name']).".png") ? 
                 normalizeString($search_platform[0]['name']).".png" :
                 "other.png";
     }
-    elseif (isset($_POST['edit_platform'])) {
+    elseif (isset($_POST['edit_platform'])) {   //Acción editar plataforma
         $search_platform  = $_SESSION['instance_platforms']->getPlatformById( $_POST['id'] );
         $categories       = $_SESSION['instance_platforms']->getCategoryIdByName( $search_platform[0]['category'] );
         $existSubcategory = $_SESSION['instance_platforms']->validateSubcategory( $categories, $_POST['subcategories'] );
@@ -98,8 +100,10 @@
         } else //Si la subcategoría no es válida
             $actionFailed = TRUE;
     }
-    elseif (isset($_POST['delete_platform'])) {
+    elseif (isset($_POST['delete_platform'])) {     //Acción de borrar la plataforma
         $search_platform = $_SESSION['instance_platforms']->getPlatformById( $_POST['idPlatform'] );
+
+        if (!sizeof($search_platform)) header('Location:./platforms.php'); //Si no existe la plataforma redirigimos a la vista main
 
         //Si la plataforma no es 'Other' procedemos a borrarla (Other es el nombre de la plataforma por defecto para cada subcategoría)
         if (normalizeString($search_platform[0]['name']) !== 'other') {
